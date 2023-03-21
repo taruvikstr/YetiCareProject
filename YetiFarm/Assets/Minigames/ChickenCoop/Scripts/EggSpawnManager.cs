392931a7f1553a88638f594eb1f8c84cb14481a9
Elias Viro <eliasviro@gmail.com>
2023-03-21 14:26:20 +0200

    Egg spawn manager.

--- YetiFarm/Assets/Minigames/ChickenCoop/Scripts/EggSpawnManager.cs
+++ YetiFarm/Assets/Minigames/ChickenCoop/Scripts/EggSpawnManager.cs
@@ -9,20 +9,45 @@ public class EggSpawnManager : MonoBehaviour
     public int difficulty; // Defines how difficult the game is.
     public int gameMode; // The game mode. 1 = collect defined number of eggs and try to avoid breaking any. 2 = endless, survive until three eggs are broken.
     public int basketAmount; // The amount of collection baskets. Defines, how many eggs will spawn at once.
-    private float spawnRate; // The frequency of egg spawns in seconds.
+    private float spawnRate = 1.0f; // The frequency of egg spawns in seconds.
     private bool gameOn = false; // Whether or not the game is still going or not.
     public int desiredScore; // The score that the player is trying to achieve.
-    public int failedEggs; // The amount of eggs that missed the baskets and broke on the ground.
+    private int failedEggs; // The amount of eggs that missed the baskets and broke on the ground.
+    private IEnumerator coroutine; // The coroutine that handles spawning egg waves.
 
+
+    private void Start()
+    {
+        coroutine = DoSpawns();
+    }
 
     public void StartEggSpawns()
     {
         score = 0;
         gameOn = true;
-        StartCoroutine(DoSpawns());
+        StartCoroutine(coroutine);
     }
 
-    IEnumerator DoSpawns()
+    private void Update()
+    {
+        // Stops egg spawning if three eggs fall on the ground in endless mode.
+        if (failedEggs >= 3 && gameMode == 2)
+        {
+            StopCoroutine(coroutine);
+        }
+    }
+
+    public void EggFail()
+    {
+        failedEggs++;
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+    }
+
+    public IEnumerator DoSpawns()
     {
         switch (difficulty)
         {
@@ -43,23 +68,114 @@ public class EggSpawnManager : MonoBehaviour
         }
         yield return new WaitForSeconds(3.0f);
 
-        for (int i = 0; i < desiredScore; i++)
+        // Integers for randomizing the player that gets their egg spawned first in a wave.
+        int playerRandomizer1;
+        int playerRandomizer2;
+
+        // Integers for randomizing which chickens lay eggs.
+        int r1;
+        int r2;
+        int r3;
+        float timeDelay1;
+        float timeDelay2;
+        while (!(((failedEggs >= 3) && gameMode == 2) || ((score >= desiredScore) && gameMode == 1) || gameOn == false))
         {
-            if (gameOn == false)
+            timeDelay1 = ((float) Random.Range(1, 10)) / 10;
+            timeDelay2 = ((float) Random.Range(1, 10)) / 10;
+            // Stop the game if three eggs are broken in endless mode or if the player has collected the desired amount of eggs.
+            if (((failedEggs >= 3) && gameMode == 2) || ((score >= desiredScore) && gameMode == 1) || gameOn == false)
             {
+                gameOn = false;
                 break;
             }
-            int r1 = Random.Range(0, 2);
-            int r2 = Random.Range(2, 4);
-            int r3 = Random.Range(4, 6);
-            int r4 = Random.Range(6, 8);
-            int r5 = Random.Range(8, 10);
-            eggSpawnerList[r1].GetComponent<SpawnEgg>().SpawnSingleEgg();
-            eggSpawnerList[r2].GetComponent<SpawnEgg>().SpawnSingleEgg();
-            eggSpawnerList[r3].GetComponent<SpawnEgg>().SpawnSingleEgg();
-            eggSpawnerList[r4].GetComponent<SpawnEgg>().SpawnSingleEgg();
-            eggSpawnerList[r5].GetComponent<SpawnEgg>().SpawnSingleEgg();
-            yield return new WaitForSeconds(2.5f);
+
+            // Randomize the laying order of chickens depending on the amount of baskets.
+            if (basketAmount == 1)
+            {
+                r1 = Random.Range(0, 12);
+                eggSpawnerList[r1].GetComponent<SpawnEgg>().SpawnSingleEgg();
+            }
+            else if (basketAmount == 2)
+            {
+                r1 = Random.Range(0, 6);
+                r2 = Random.Range(6, 12);
+                playerRandomizer1 = Random.Range(0, 2);
+                if (playerRandomizer1 == 1)
+                {
+                    eggSpawnerList[r1].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                    yield return new WaitForSeconds(timeDelay1);
+                    eggSpawnerList[r2].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                }
+                else
+                {
+                    eggSpawnerList[r2].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                    yield return new WaitForSeconds(timeDelay1);
+                    eggSpawnerList[r1].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                }
+            }
+            else
+            {
+                r1 = Random.Range(0, 4);
+                r2 = Random.Range(4, 8);
+                r3 = Random.Range(8, 12);
+                playerRandomizer1 = Random.Range(0, 3);
+                playerRandomizer2 = Random.Range(0, 2);
+                if (playerRandomizer2 == 1)
+                {
+                    if (playerRandomizer1 == 1)
+                    {
+                        eggSpawnerList[r1].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                        yield return new WaitForSeconds(timeDelay1);
+                        eggSpawnerList[r2].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                        yield return new WaitForSeconds(timeDelay2);
+                        eggSpawnerList[r3].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                    }
+                    else if (playerRandomizer1 == 2)
+                    {
+                        eggSpawnerList[r1].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                        yield return new WaitForSeconds(timeDelay1);
+                        eggSpawnerList[r3].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                        yield return new WaitForSeconds(timeDelay2);
+                        eggSpawnerList[r2].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                    }
+                    else
+                    {
+                        eggSpawnerList[r2].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                        yield return new WaitForSeconds(timeDelay1);
+                        eggSpawnerList[r1].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                        yield return new WaitForSeconds(timeDelay2);
+                        eggSpawnerList[r3].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                    }
+                }
+                else
+                {
+                    if (playerRandomizer1 == 1)
+                    {
+                        eggSpawnerList[r2].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                        yield return new WaitForSeconds(timeDelay1);
+                        eggSpawnerList[r3].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                        yield return new WaitForSeconds(timeDelay2);
+                        eggSpawnerList[r1].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                    }
+                    else if (playerRandomizer1 == 2)
+                    {
+                        eggSpawnerList[r3].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                        yield return new WaitForSeconds(timeDelay1);
+                        eggSpawnerList[r2].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                        yield return new WaitForSeconds(timeDelay2);
+                        eggSpawnerList[r1].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                    }
+                    else
+                    {
+                        eggSpawnerList[r3].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                        yield return new WaitForSeconds(timeDelay1);
+                        eggSpawnerList[r1].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                        yield return new WaitForSeconds(timeDelay2);
+                        eggSpawnerList[r2].GetComponent<SpawnEgg>().SpawnSingleEgg();
+                    }
+                }
+            }
+            yield return new WaitForSeconds(spawnRate);
         }
     }
 }
