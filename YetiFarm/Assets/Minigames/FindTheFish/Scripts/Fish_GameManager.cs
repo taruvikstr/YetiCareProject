565b7f8ada6a2642e4a78186d5c08d005b4f688b
Taru Vikström <taru.vikstrom@gmail.com>
2023-03-22 21:06:55 +0200

    Fish game, fish instantiating at the start of the game

--- YetiFarm/Assets/Minigames/FindTheFish/Scripts/Fish_GameManager.cs
+++ YetiFarm/Assets/Minigames/FindTheFish/Scripts/Fish_GameManager.cs
@@ -6,11 +6,20 @@ public class Fish_GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject fishPrefab;
     [SerializeField] public List<GameObject> fishInstances;
+    [SerializeField] private List<GameObject> spawnpoints;
 
+    private void Start()
+    {
+        StartGame();    
+    }
 
     public void StartGame()
     {
-
+        foreach(GameObject spawn in spawnpoints)
+        {
+            GameObject fishInstance = Instantiate(fishPrefab, spawn.transform);
+            fishInstances.Add(fishInstance);
+        }
     }
     public void ResetGame()
     {
