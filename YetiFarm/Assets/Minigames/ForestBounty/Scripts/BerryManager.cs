7186bbefdc938523e29666660f0f2b479b2e78a4
Lilian Aasa <91128698+AALilian@users.noreply.github.com>
2023-03-23 15:48:35 +0200

    Berry drag movement script + berry sprite import

---
+++ YetiFarm/Assets/Minigames/ForestBounty/Scripts/BerryManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BerryManager : MonoBehaviour
+{
+    // täs skriptis ei oo mitää fiksuu viel. läjä kokeilui vaa. 
+
+    //public GameObject strawberryBasket;
+    //public GameObject blueberryBasket;
+    //public GameObject raspberryBasket;
+    //public GameObject cowberryBasket;
+
+    //[SerializeField] private GameObject strawberry;
+    //[SerializeField] private GameObject blueberry;
+    //[SerializeField] private GameObject raspberry;
+    //[SerializeField] private GameObject cowberry;
+
+    //private Vector2 offset;
+    //private bool dragging = false;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    // Move objects
+    //    if (Input.touchCount > 0)
+    //    {
+    //        Touch touch = Input.GetTouch(0);
+
+    //        switch (touch.phase)
+    //        {
+    //            case TouchPhase.Began:
+                    
+    //                offset = transform.position - Camera.main.ScreenToWorldPoint(touch.position);
+    //                dragging = true;
+    //                break;
+
+    //            case TouchPhase.Moved:
+    //                if (dragging)
+    //                {
+    //                    Vector2 newPos = Camera.main.ScreenToWorldPoint(touch.position);
+    //                    transform.position = newPos + offset;
+    //                }
+    //                break;
+
+    //            case TouchPhase.Ended:
+    //                dragging = false;
+    //                break;
+    //        }
+    //    }
+    //}
+
+   
+
+}
