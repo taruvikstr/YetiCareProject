6638888c5fc7ab3091ccc7b50dd0e359db757ba0
DanielH1337 <62145497+DanielH1337@users.noreply.github.com>
2023-03-24 16:11:06 +0200

    bugfix WoodChopGame

--- YetiFarm/Assets/Minigames/WoodNinja/Scripts/Spawner.cs
+++ YetiFarm/Assets/Minigames/WoodNinja/Scripts/Spawner.cs
@@ -9,15 +9,25 @@ public class Spawner : MonoBehaviour
     
     public GameObject prefab;
 
+    private bool isSpawned=true;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-         if (other.tag == "Player")
+         if (other.tag == "Player" && isSpawned)
          {
-             Instantiate(prefab, transform.position, transform.rotation);
+             Instantiate(prefab, spawnPoint.transform.position, transform.rotation);
              Debug.Log("HIT");
+            isSpawned = false;
+               
          }
 
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player" && !isSpawned)
+        {
+            isSpawned = true;
+        } 
     }
     private void OnMouseDown()
     {
