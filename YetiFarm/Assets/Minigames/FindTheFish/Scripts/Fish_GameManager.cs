754307a54218646274fa38f3a0acc3b6568821d1
Taru Vikström <taru.vikstrom@gmail.com>
2023-03-26 12:14:11 +0300

    FishGame- DiceRolling

--- YetiFarm/Assets/Minigames/FindTheFish/Scripts/Fish_GameManager.cs
+++ YetiFarm/Assets/Minigames/FindTheFish/Scripts/Fish_GameManager.cs
@@ -7,6 +7,7 @@ public class Fish_GameManager : MonoBehaviour
     [SerializeField] private GameObject fishPrefab;
     [SerializeField] public List<GameObject> fishInstances;
     [SerializeField] private List<GameObject> spawnpoints;
+    [SerializeField] private SpriteRenderer dicePrimary, diceSecondary, dicePattern;
 
     private void Start()
     {
@@ -20,7 +21,17 @@ public class Fish_GameManager : MonoBehaviour
             GameObject fishInstance = Instantiate(fishPrefab, spawn.transform);
             fishInstances.Add(fishInstance);
         }
+
     }
+
+    public void RollDice()
+    {
+        GameObject chosenFish = fishInstances[Random.Range(0, fishInstances.Count)];
+        dicePrimary.color = chosenFish.GetComponent<FishController>().primaryColor[0];
+        diceSecondary.color = chosenFish.GetComponent<FishController>().secondaryColor[0];
+        dicePattern.sprite = chosenFish.GetComponent<FishController>().pattern[0];
+    }
+
     public void ResetGame()
     {
 
